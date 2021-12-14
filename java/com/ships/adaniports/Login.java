d553385bd70f6a2885a6232640402f967d70df57
AnuGayathri <84897361+AnuGayathri@users.noreply.github.com>
2021-12-14 10:11:40 +0530

    javafiles
    It contains java classes, interfaces and servlet files

---
+++ java/com/ships/adaniports/Login.java
@@ -0,0 +1,31 @@
+package com.ships.adaniports;
+public class Login {
+
+	private String username;
+	private String password;
+    private String name;
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+
+	
+
+}
